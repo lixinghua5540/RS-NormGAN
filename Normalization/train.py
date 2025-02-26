00abcbcd16f3ff86ce1330f152b9704ed7157cff
Xinghua Li <75232301+lixinghua5540@users.noreply.github.com>
2025-02-26 22:21:12 +0800

    Update train.py

--- Normalization/train.py
+++ Normalization/train.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
                 model.save_networks(save_suffix)
 
             iter_data_time = time.time()
-        #opt.save_epoch_freq=1
+
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
             model.save_networks('latest')
